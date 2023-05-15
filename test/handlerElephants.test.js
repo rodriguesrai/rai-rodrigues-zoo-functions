c6c74e8ed4b30187925e7a7a1092dc6d9e6c0a7d
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-05-15 15:38:08 -0300

    requisito 6 completo

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,16 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Verifica se retorna undefined quando não inserido parâmetros', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  test('Verifica se retorna parâmetro inválido quano não é uma string.', () => {
+    expect(handlerElephants(54)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  test('Verifica se retorna o valor ao adicionar uma chave', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  test('Verifica se count retorna o valor 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+});
