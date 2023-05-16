2d154fb67b257a3195534759bfe9bf0831ebdbe8
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-05-16 10:35:49 -0300

    requisito 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 };
 
-const calculateEntry = (entrants) => {
-  // seu código aqui
+const calculateEntry = (entrants = []) => {
+  const entries = countEntrants(entrants);
+  const valorChild = entries.child * 20.99;
+  const valorAdult = entries.adult * 49.99;
+  const valorSenior = entries.senior * 24.99;
+  const sum = valorChild + valorAdult + valorSenior;
+  const finalResult = +(sum.toFixed(2));
+  return entrants.length === 0 ? 0 : finalResult;
 };
 
 module.exports = { calculateEntry, countEntrants };
