723947335c530b22d49d150c8a250020ec140986
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-05-16 11:49:33 -0300

    requisito 10 completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const dadosColaborador = data.employees.find((elemento) => elemento.id === id);
+  const animalColaborador = dadosColaborador.responsibleFor[0];
+  const especieAnimal = data.species.find((elemento) => elemento.id === animalColaborador);
+  const animaisDaEspecie = especieAnimal.residents;
+  const animalMaisAntigo = animaisDaEspecie.reduce(
+    (maisAntigo, animal) =>
+      (animal.age > maisAntigo.age ? animal : maisAntigo),
+  );
+
+  return [animalMaisAntigo.name, animalMaisAntigo.sex, animalMaisAntigo.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+// const ScheduleAnimals = (animal) => {
+//   const animalSpecies = data.species.find((species) => species.name === animal)
+//   return animalSpecies.availability;
+// };
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  // {
+  //  Monday:
+  //  Tuesday:
+  //  Wednesday:
+  //  Thursday:
+  //  Friday:
+  //  Saturday:
+  //  Sunday:
+  // }
 };
 
 module.exports = getSchedule;
